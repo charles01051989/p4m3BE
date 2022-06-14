8fdea3fd86c42b90f3f2ce93f85b7d5d560371b0
Charles Marques <charles_sjp@yahoo.com.br>
2022-06-14 18:47:49 -0300

    ok

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const app = express();
 
 const cors = require('cors');
